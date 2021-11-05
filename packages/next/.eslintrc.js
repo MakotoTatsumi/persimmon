b5f4906281061ba19252cb9a5c37c1b668cceede
MakotoTatsumi <sakuchan.jp@gmail.com>
2021-11-05 16:27:57 +0900

    add: eslint settings

---
+++ packages/next/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+  },
+  extends: [
+    "airbnb",
+    "./configs/eslint/js",
+    "./configs/eslint/unicorn",
+    "./configs/eslint/import",
+    "./configs/eslint/next",
+  ],
+}
+
+
+
+
