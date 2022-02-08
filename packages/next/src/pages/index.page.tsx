8fe61cd4a193903dec294d996538c0a6cce57f7f
MakotoTatsumi <sakuchan.jp@gmail.com>
2022-02-08 17:37:22 +0900

    feat(next): add data-scheme

---
+++ packages/next/src/pages/index.page.tsx
@@ -0,0 +1,42 @@
+import type { NextPage } from "next"
+import { apiClient } from "../shared/utils/apiClient";
+import { format } from 'date-fns'
+import { Methods } from "../libs/apiClient/blogs";
+
+type BlogsGetResult = {
+  status: number
+  body: Methods["get"]["resBody"]
+}
+
+const Home: NextPage = (props: any) => {
+  const { articleData } = props
+  console.log(articleData)
+
+  return <div>success: {props.name}</div>
+}
+
+export default Home
+
+const getDisplayDate = (targetOriginData?: string): string => {
+  return targetOriginData || ""
+}
+
+export async function getStaticProps() {
+  // openapi2aspida での定義がおかしいっぽい
+  // @ts-ignore
+  const articleData = await (apiClient.blogs.get() as Promise<BlogsGetResult>)
+
+  const dateConvertedData = articleData.body.map(data => {
+    return {
+      ...data,
+      publishedAt: data.published_at ? format(new Date(data.published_at), "yy.MM.dd HH:mm") : ""
+    }
+  })
+
+  return {
+    props: {
+      name: "パーシモン",
+      articleData: dateConvertedData
+    },
+  }
+}
